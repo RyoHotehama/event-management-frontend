c501fa233c9f088bded8fdd695e22cc8b1568f93
Hotehama2022 <hotehama-r@cin-group.com>
2024-07-07 18:38:02 +0900

    ページの色変更

--- src/components/paginate.tsx
+++ src/components/paginate.tsx
@@ -1,9 +1,19 @@
 'use client'
 import { onClickPage } from "@/hooks/profileListHooks"
-import { Pagination } from "@mui/material"
+import { Pagination, styled } from "@mui/material"
+
+const CustomPagination = styled(Pagination)(({ theme }) => ({
+    '& .MuiPaginationItem-root': {
+      color: 'green',
+    },
+    '& .MuiPaginationItem-page.Mui-selected': {
+      backgroundColor: 'green',
+      color: 'white',
+    },
+  }));
 
 export const Paginate = ({profileList, role}: {profileList: ProfileList, role: number}) => {
     return (
-        <Pagination count={profileList.last_page} color="primary" page={profileList.current_page} onChange={(event, page) => onClickPage(page, role)}/>
+        <CustomPagination count={profileList.last_page} color="primary" page={profileList.current_page} onChange={(event, page) => onClickPage(page, role)}/>
     )
 }
